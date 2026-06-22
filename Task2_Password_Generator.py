25de1f1663d8f2519f0f87aa464ff1de03483c69
mauryashagun829-creator <mauryashagun829@gmail.com>
2026-06-22 22:42:16 +0530

    Added Task 2 - Password generator
    The password generator now requires user-defined counts for each character type, ensuring the total matches the specified length. It also includes error handling for mismatched character counts.

--- Task2_Password_Generator.py
+++ Task2_Password_Generator.py
@@ -1,1 +1,41 @@
+import random
+import string
 
+print("===== Password Generator =====")
+
+length = int(input("Enter Total Password Length: "))
+
+upper = int(input("Enter number of Uppercase Letters: "))
+lower = int(input("Enter number of Lowercase Letters: "))
+digits = int(input("Enter number of Digits: "))
+special = int(input("Enter number of Special Characters: "))
+
+if upper + lower + digits + special != length:
+    print("\nError: Sum of all character types must be equal to password length!")
+else:
+    password = []
+
+    # Uppercase letters
+    for i in range(upper):
+        password.append(random.choice(string.ascii_uppercase))
+
+    # Lowercase letters
+    for i in range(lower):
+        password.append(random.choice(string.ascii_lowercase))
+
+    # Digits
+    for i in range(digits):
+        password.append(random.choice(string.digits))
+
+    # Special characters
+    for i in range(special):
+        password.append(random.choice(string.punctuation))
+
+    # Shuffle password
+    random.shuffle(password)
+
+    # Convert list to string
+    password = "".join(password)
+
+    print("\nGenerated Password:", password)
+    print("Password Length:", len(password))
